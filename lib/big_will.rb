f8fad44d4798991eca3e37bc3c204ef631095bcd
Benjamin Kollek <benkollek@gmail.com>
2015-03-20 13:12:32 +0000

    Submission

--- lib/big_will.rb
+++ lib/big_will.rb
@@ -1,1 +1,24 @@
-#Your code goes here
+class BigWill
+  attr_accessor :text
+
+  def initialize string
+    @text = string
+  end
+
+  def process
+    will = 'will'
+    text_index = 0
+    (0..will.length-1).each do |character1|
+      (text_index..text.length).each do |i|
+        text_index = i
+        if self.text[i] == character1
+          puts 'inserted!'
+          text.insert(i-1, '(')
+          text.insert(i+2, ')')
+        end
+      end
+    end
+    text
+  end
+
+end
